77c55ec5bd6480aca48d3ba4cf751b1c533ead96
Raphael Chagas Bernardes <rchagas@users.noreply.github.com>
2023-07-17 19:58:07 +0000

    Add prettier and `lint:fix` and `lint:check` scripts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,23 @@
-import * as React from 'react';
+import * as React from "react";
 
 export default function Home() {
-    return (
-        <div style={{margin: '10px'}}>
-            <h3>
-                Mesmo que os dias bons pareça ter ficado para trás
-            </h3>
-            <h4>
-                Mesmo que a gente pareça dois estranhos
-            </h4>
-            <h5>
-                Mesmo que a distância pareça grande demais
-            </h5>
-            <h6>
-                Mesmo que eu pareça frio e insensível às vezes
-            </h6>
-            <h6>
-                Mesmo que pareça que seja melhor apenas desistir
-            </h6>
-            <h2>
-                Ainda quero estar do seu lado de manhã.
-            </h2>                 
-            <iframe style={{border: 'none'}}
-                src="https://open.spotify.com/embed/track/429NtPmr12aypzFH3FkN9l?utm_source=generator&theme=0" 
-                width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
-                </iframe>
-        </div>
-    );
+  return (
+    <div style={{ margin: "10px" }}>
+      <h3>Mesmo que os dias bons pareça ter ficado para trás</h3>
+      <h4>Mesmo que a gente pareça dois estranhos</h4>
+      <h5>Mesmo que a distância pareça grande demais</h5>
+      <h6>Mesmo que eu pareça frio e insensível às vezes</h6>
+      <h6>Mesmo que pareça que seja melhor apenas desistir</h6>
+      <h2>Ainda quero estar do seu lado de manhã.</h2>
+      <iframe
+        style={{ border: "none" }}
+        src="https://open.spotify.com/embed/track/429NtPmr12aypzFH3FkN9l?utm_source=generator&theme=0"
+        width="100%"
+        height="152"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
 }
